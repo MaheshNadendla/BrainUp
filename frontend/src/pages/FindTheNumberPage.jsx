0cf543ba704f760eaa510d1d5fb2d9469759d0f7
MaheshNadendla <maheshnadendla147@gmail.com>
2026-01-09 17:35:10 +0530

    next5

--- frontend/src/pages/FindTheNumberPage.jsx
+++ frontend/src/pages/FindTheNumberPage.jsx
@@ -4,13 +4,13 @@ import FindTheNumberGame from '../components/FindTheNumberGame.jsx'
 
 function FindTheNumberPage() {
   return (
-    // <FindTheNumberGame
-    //   targetNumber={42} 
-    //   commonNumber={24} 
-    //   distractors={[23, 29, 25]} 
-    //   title="BrainUp Games"
-    // />
-    <FindTheNumberGame/>
+    <FindTheNumberGame
+      targetNumber={42} 
+      commonNumber={24} 
+      distractors={[23, 29, 25]} 
+      title="BrainUp Games"
+    />
+    // <FindTheNumberGame/>
   )
 }
 
